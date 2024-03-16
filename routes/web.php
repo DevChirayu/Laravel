8839062b7233bf899dbcd029e69d1531a34befce
DevChirayu <chirayupatil23@gmail.com>
2024-03-16 22:36:02 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/jenkins', function () {
-    return 'Jenkins build success with webhook test';
+    return 'Jenkins build success with webhook test poll';
 });
